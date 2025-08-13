00e18b83d8b686025c192fdd0eb48f742ff2d6af
Christopher House <chrishouse180@gmail.com>
2025-08-12 17:34:25 -0700

    Short description of commit
    Home page component added
    
    Longer explanation of the motivation for the change
    
    Created a dedicated Home.jsx component as well as initial layout concept and structure.
    Fixes-Bug: Enter bug-id or delete line
    
    Implements-Requirement: Enter requirement-id or delete line

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import { Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import AuthPage from './components/AuthPage';
+
 
 
 export default function App() {
-
   return (
-    <>
-      
-    </>
-  )
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path="/auth" element={<AuthPage />} />
+    </Routes>
+  );
 }
