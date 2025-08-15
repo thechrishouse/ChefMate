6b9d5abe7e9015cf05963b5310d3c6039c9a90c8
binubims <binubimshara2015@gmail.com>
2025-08-14 21:53:45 -0700

    trying to fix the fronend user authentication

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import AuthPage from './components/AuthPage';
+import Recipes from './components/Recipes';
 
 
 
@@ -8,7 +9,8 @@ export default function App() {
   return (
     <Routes>
       <Route path='/' element={<Home />} />
-      <Route path="/auth" element={<AuthPage />} />
+      <Route path='/auth' element={<AuthPage />} />
+      <Route path='/recipes' element={<Recipes />} />
     </Routes>
   );
 }
