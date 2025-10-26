07f739c4c27ed66d01557e511ee2a8b5f6ee94f7
Christopher House <chrishouse180@gmail.com>
2025-10-26 00:58:07 -0500

    updated header styles

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -4,16 +4,16 @@ import { PiChefHat, PiChefHatBold } from "react-icons/pi";
 export default function Header() {
 
     // Dynamically changes link styles bases on active/inactive states
-    const linkClasses = "transition px-2 py-1 rounded-md";
-    const activeClasses = "text-black font-semibold border-b-2 border-orange-500";
+    const linkClasses = "transition text-lg px-2 py-1 rounded-md";
+    const activeClasses = "text-black text-lg font-semibold border-b-2 border-orange-500";
 
 
     return (
         <header id='header' className='z-50 sticky top-0 left-0 w-full flex justify-between items-center py-4 px-10 md:px-18 text-gray-900  backdrop-blur-md shadow-[0px_4px_16px_rgba(0,0,0,0.3)]'>
             <div>
                 <NavLink to='/' className='flex justify-between items-center space-x-4 text-green-700'>
-                    <PiChefHatBold size={52} className='cursor-pointer' />
-                    <h1 className='font-bold text-2xl cursor-pointer'>ChefMate</h1>
+                    <PiChefHatBold size={50} className='cursor-pointer' />
+                    <h1 className='font-bold text-4xl cursor-pointer'>ChefMate</h1>
                 </NavLink>
             </div>
 
